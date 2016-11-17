f7a2c55b0e93cf03eb1485faf451d0a5c1d47847
viko16 <16viko@gmail.com>
2016-11-18 00:30:09 +0800

    chore: 修改 webpack config 缩进

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -6,85 +6,85 @@ import autoprefixer from 'autoprefixer';
 const ENV = process.env.NODE_ENV || 'development';
 
 module.exports = {
-	entry: './src/index.js',
+  entry: './src/index.js',
 
-	output: {
-		path: './build',
-		publicPath: '/',
-		filename: 'bundle.js'
-	},
+  output: {
+    path: './build',
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
 
-	resolve: {
-		extensions: ['', '.jsx', '.js', '.json', '.less']
-	},
+  resolve: {
+    extensions: ['', '.jsx', '.js', '.json', '.less']
+  },
 
-	module: {
-		preLoaders: [
-			{
-				test: /\.jsx?$/,
-				exclude: /src\//,
-				loader: 'source-map'
-			}
-		],
-		loaders: [
-			{
-				test: /\.jsx?$/,
-				exclude: /node_modules/,
-				loader: 'babel'
-			},
-			{
-				test: /\.(less|css)$/,
-				loader: ExtractTextPlugin.extract('css?sourceMap!postcss!less?sourceMap')
-			},
-			{
-				test: /\.json$/,
-				loader: 'json'
-			},
-			{
-				test: /\.(xml|html|txt)$/,
-				loader: 'raw'
-			},
-			{
-				test: /\.(svg|woff|ttf|eot)(\?.*)?$/i,
-				loader: 'file-loader?name=assets/fonts/[name]_[hash:base64:5].[ext]'
-			}
-		]
-	},
+  module: {
+    preLoaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /src\//,
+        loader: 'source-map'
+      }
+    ],
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel'
+      },
+      {
+        test: /\.(less|css)$/,
+        loader: ExtractTextPlugin.extract('css?sourceMap!postcss!less?sourceMap')
+      },
+      {
+        test: /\.json$/,
+        loader: 'json'
+      },
+      {
+        test: /\.(xml|html|txt)$/,
+        loader: 'raw'
+      },
+      {
+        test: /\.(svg|woff|ttf|eot)(\?.*)?$/i,
+        loader: 'file-loader?name=assets/fonts/[name]_[hash:base64:5].[ext]'
+      }
+    ]
+  },
 
-	postcss: () => [
-		autoprefixer({ browsers: 'last 2 versions' })
-	],
+  postcss: () => [
+    autoprefixer({ browsers: 'last 2 versions' })
+  ],
 
-	plugins: ([
-		new webpack.NoErrorsPlugin(),
-		new ExtractTextPlugin('style.css', { allChunks: true }),
-		new webpack.DefinePlugin({
-			'process.env.NODE_ENV': JSON.stringify(ENV)
-		}),
-		new HtmlWebpackPlugin()
-	]).concat(ENV==='production' ? [
-		new webpack.optimize.DedupePlugin(),
-		new webpack.optimize.OccurenceOrderPlugin()
-	] : []),
+  plugins: ([
+    new webpack.NoErrorsPlugin(),
+    new ExtractTextPlugin('style.css', { allChunks: true }),
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify(ENV)
+    }),
+    new HtmlWebpackPlugin()
+  ]).concat(ENV === 'production' ? [
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.OccurenceOrderPlugin()
+  ] : []),
 
-	stats: { colors: true },
+  stats: { colors: true },
 
-	devtool: ENV==='production' ? 'source-map' : 'inline-source-map',
+  devtool: ENV === 'production' ? 'source-map' : 'inline-source-map',
 
-	devServer: {
-		port: process.env.PORT || 8080,
-		host: '0.0.0.0',
-		colors: true,
-		publicPath: '/',
-		contentBase: './src',
-		historyApiFallback: true,
-		proxy: [
-			// OPTIONAL: proxy configuration:
-			// {
-			// 	path: '/optional-prefix/**',
-			// 	target: 'http://target-host.com',
-			// 	rewrite: req => { req.url = req.url.replace(/^\/[^\/]+\//, ''); }   // strip first path segment
-			// }
-		]
-	}
+  devServer: {
+    port: process.env.PORT || 8080,
+    host: '0.0.0.0',
+    colors: true,
+    publicPath: '/',
+    contentBase: './src',
+    historyApiFallback: true,
+    proxy: [
+      // OPTIONAL: proxy configuration:
+      // {
+      // 	path: '/optional-prefix/**',
+      // 	target: 'http://target-host.com',
+      // 	rewrite: req => { req.url = req.url.replace(/^\/[^\/]+\//, ''); }   // strip first path segment
+      // }
+    ]
+  }
 };
